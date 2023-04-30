585f092cb55bf6cec4d3118a097026f5ae9f36db
CodeSandbox <support@codesandbox.io>
2023-04-30 16:19:06 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const form = document.getElementById("generate-form");
+const qr = document.getElementById("qrcode");
+
+const onGenerateSubmit = (e) => {
+  e.preventDefault();
+
+  const url = document.getElementById("url").value;
+  const size = document.getElementById("size").value;
+
+  if (url === " ") {
+    alert("Please enter a valid URL");
+  } else {
+    showSpinner();
+
+    setTimeout(() => {
+      hideSpinner();
+
+      generateQRCode(url, size);
+    }, 3000);
+  }
+};
+
+const generateQRCode = (url, size) => {
+  const qrcode = new QRCode("qrcode", {
+    text: url,
+    width: size,
+    height: size,
+  });
+};
+
+const showSpinner = () => {
+  document.getElementById("spinner").style.display = "block";
+};
+
+const hideSpinner = () => {
+  document.getElementById("spinner").style.display = "none";
+};
+
+hideSpinner();
+
+form.addEventListener("submit", onGenerateSubmit);
